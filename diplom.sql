fd784e5a0ca8d3752b5f0f19ac0059079f16c753
P4elme6ka <006gta1@gmail.com>
2022-06-15 03:15:34 +0300

    some comit

--- diplom.sql
+++ diplom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 10 2022 г., 08:54
+-- Время создания: Июн 15 2022 г., 03:14
 -- Версия сервера: 8.0.29
 -- Версия PHP: 7.4.29
 
@@ -38,8 +38,7 @@ CREATE TABLE `acceptance` (
 --
 
 INSERT INTO `acceptance` (`id`, `year`, `is_open`) VALUES
-(1, 2020, 0),
-(2, 2022, 1);
+(4, 2020, 1);
 
 -- --------------------------------------------------------
 
@@ -60,10 +59,9 @@ CREATE TABLE `acceptance_class` (
 --
 
 INSERT INTO `acceptance_class` (`id`, `acceptance_id`, `name`, `description`, `number_seats`) VALUES
-(1, 1, 'Монтажники', 'Ебать его в рот какая крутая специальность, поступайте к нам и в хуй не дуйте', 0),
-(2, 1, 'Программисты', 'Программисты конечно не монтажники, но им тоже вроде на хлеб хватать должно, ну и в хуй они иногда дуют.', 0),
-(3, 1, 'Монтажники программисты', 'Самая странная специальность', 32),
-(4, 2, 'Монтажники программисты', 'фывфывфывфыв', 25);
+(6, 4, 'Программисты', 'Описание специальности - программисты', 75),
+(7, 4, 'Монтажники', 'Описание специальности - монтажники', 25),
+(8, 4, 'Системные администраторы', 'Описание специальности - администраторы', 25);
 
 -- --------------------------------------------------------
 
@@ -82,12 +80,15 @@ CREATE TABLE `file` (
 --
 
 INSERT INTO `file` (`id`, `name`, `path`) VALUES
-(1, 'Заявление', 'uploads/horse_logo.jpg'),
-(2, 'Согласие на обработку данных', 'uploads/ava31k.png'),
-(3, 'Аттестат', 'uploads/e9d1ea51c598a3d48f16e43707d9f974.jpg'),
-(4, 'ava4.png', 'uploads/TSXQu218-A0T46Aot2ClUlF2t1zfTXjc.png'),
-(5, 'ava31k.png', 'uploads/tWDy_3kns_l5dwIjE0ZlGarOmXMWByOe.png'),
-(6, 'e9d1ea51c598a3d48f16e43707d9f974.jpg', 'uploads/6KmoDcpNMN-LiTdG-rWBIlHcHT9ylUTu.jpg');
+(1, 'jag.png', 'uploads/OHraPIQSpCl6iTaJtOL53EFy8CtpjKj0.png'),
+(2, 'photo_2021-02-06_16-05-19.jpg', 'uploads/Ca5GSjafavaeee5Qvd1Cr8vC5hqWEV89.jpg'),
+(3, 'myphoto (2).jpg', 'uploads/4qYlr2sI8PKr_T8zSAGrM5L7VMiB13hl.jpg'),
+(4, 'pasp.jpg', 'uploads/hk5b8AYbZduNlsqX5APIkY6vxQBX9PfV.jpg'),
+(5, 'ava3.png', 'uploads/ZhmgAQShHjdWGpoGxVfeSswAYO1c2lAx.png'),
+(6, 'e9d1ea51c598a3d48f16e43707d9f974.jpg', 'uploads/bThsFbERT0R7VSek-PgcUsYq-HA20mQe.jpg'),
+(7, 'horse_logo.jfif', 'uploads/W9QB3M_EbSCl7J30BI1L0gBogAuWGHUK.jfif'),
+(8, '1637500046_3-papik-pro-p-pelmen-risunok-5.jpg', 'uploads/jP84iCDeVCRf-FF0CiNctYEAgObIKmTs.jpg'),
+(9, 'e9d1ea51c598a3d48f16e43707d9f974.jpg', 'uploads/XUScfs1YVS173xti5eN8d4EMdPNoZVxK.jpg');
 
 -- --------------------------------------------------------
 
@@ -142,7 +143,6 @@ CREATE TABLE `user` (
   `age` int DEFAULT NULL,
   `city` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `status_id` int NOT NULL,
-  `acceptance_id` int DEFAULT NULL,
   `access_token` varchar(255) NOT NULL,
   `fio` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -153,11 +153,10 @@ CREATE TABLE `user` (
 -- Дамп данных таблицы `user`
 --
 
-INSERT INTO `user` (`id`, `role_id`, `email`, `public_status`, `phone`, `age`, `city`, `status_id`, `acceptance_id`, `access_token`, `fio`, `password`, `class_id`) VALUES
-(1, 2, '006gta1@gmail.com', '-', '89110229765', 20, 'Санкт-Петербург', 1, 1, 'j4ZT96C7UE8UhJuJsXeJ74KRM-G8fnMVQ3kzaV4S8NcJcmmYZu57IgQhbJyFMvkw', 'Корполевский Артем Кириллович', '$2y$13$gp0STN.a.wMarThBW06OhOkQo8l5O01KOreURl7IzzelE1h3Se7ku', NULL),
-(2, 1, 'student@rtk.ru', '-', '89110229765', 12, 'Санкт-Петербург', 1, 1, 'ph0yxwOE2Lm4Qr2MXnPEG7SR3JFZyJSKXxtRgRbweYjnV3GUSeVGsDK4ygH4hCt_', 'Студент Студентов Студентович', '$2y$13$GdQQtXq/k9rBR/hhrLReX.5WlSTs3zSza1s.u3tLI6brcuYZAZ9oe', 1),
-(3, 1, 'student@rtk.ru', '-', '89110229765', 13, 'Санкт-Петербург', 1, 1, 'm1kqcOKzm3f2KPB3wArib3L9XP24qNMIQNTEjgYPkABgSd56Nt75Cfy5UNrF9Z2P', 'Студент Студентов Студентович', '$2y$13$X/FTdNlMrNZiItF6fCkim.xzmXdNqX68Jil5Iy5VlR4GtxSOKDggK', 4),
-(4, 1, 'student@rtk.ru', '-', '89110229765', 14, 'Санкт-Петербург', 1, 1, '38-HAyunNLxzt9f46xo_pYnglJo3ZGBtkGCSQ-NqpRVahpjNkawXWymZRcwCvdks', 'Студент Студентов Студентович', '$2y$13$wDpFr0Z39.wr2QJs8sHhP.72NmR3zMxZbKXpC6M9hPnM6EAawunuW', 4);
+INSERT INTO `user` (`id`, `role_id`, `email`, `public_status`, `phone`, `age`, `city`, `status_id`, `access_token`, `fio`, `password`, `class_id`) VALUES
+(1, 2, 'admin@admin.admin', '-', '89110229765', 20, 'Санкт-Петербург', 1, 'j4ZT96C7UE8UhJuJsXeJ74KRM-G8fnMVQ3kzaV4S8NcJcmmYZu57IgQhbJyFMvkw', 'Корполевский Артем Кириллович', '$2y$13$gp0STN.a.wMarThBW06OhOkQo8l5O01KOreURl7IzzelE1h3Se7ku', NULL),
+(2, 1, 'student@rtk.ru', '-', '89110229765', 12, 'Санкт-Петербург', 1, 'ph0yxwOE2Lm4Qr2MXnPEG7SR3JFZyJSKXxtRgRbweYjnV3GUSeVGsDK4ygH4hCt_', 'Студент Студентов Студентович', '$2y$13$GdQQtXq/k9rBR/hhrLReX.5WlSTs3zSza1s.u3tLI6brcuYZAZ9oe', 1),
+(5, 1, 'student2@rtk.ru', '-', '89110229765', 18, 'Санкт-Петербург', 1, 'O3OarjrEMoKqJz5AggzRQrBYPS0pzDyUpdUPRLbhCuAd3KHr1ye7AE21LkVYXuga', 'Студент2 Студентов2 Студентович2', '$2y$13$QMAP9bN4CH6onagj7DTfJePHCl6Qf1i230fIzJ/HRLSxKReVVvKpC', NULL);
 
 -- --------------------------------------------------------
 
@@ -175,12 +174,15 @@ CREATE TABLE `user_acceptance_attachment` (
 --
 
 INSERT INTO `user_acceptance_attachment` (`user_request_id`, `file_id`) VALUES
-(20, 1),
-(20, 2),
-(20, 3),
-(21, 4),
-(21, 5),
-(21, 6);
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(3, 7),
+(3, 8),
+(3, 9);
 
 -- --------------------------------------------------------
 
@@ -202,17 +204,8 @@ CREATE TABLE `user_acceptance_request` (
 --
 
 INSERT INTO `user_acceptance_request` (`id`, `date`, `is_original`, `user_id`, `atestat_mean`, `acceptance_class_id`) VALUES
-(11, '2022-06-09 16:32:16', 0, 1, '5.500', 4),
-(12, '2022-06-09 16:34:46', 0, 1, '4.400', 2),
-(13, '2022-06-09 17:17:31', 0, 1, '4.400', 2),
-(14, '2022-06-09 17:18:42', 0, 1, '4.400', 2),
-(15, '2022-06-09 17:19:24', 0, 1, '5.500', 2),
-(16, '2022-06-09 17:21:31', 0, 1, '5.500', 2),
-(17, '2022-06-09 17:27:05', 0, 1, '4.400', 2),
-(18, '2022-06-09 17:28:16', 0, 1, '4.400', 2),
-(19, '2022-06-09 17:29:12', 0, 1, '4.400', 2),
-(20, '2022-06-09 17:29:27', 0, 1, '4.400', 2),
-(21, '2022-06-09 17:33:01', 0, 1, '4.400', 2);
+(1, '2022-06-14 22:18:36', 1, 2, '4.400', 6),
+(2, '2022-06-14 22:22:02', 0, 5, '4.400', 6);
 
 --
 -- Индексы сохранённых таблиц
@@ -255,7 +248,6 @@ ALTER TABLE `status`
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD KEY `status_id` (`status_id`),
-  ADD KEY `acceptance_id` (`acceptance_id`),
   ADD KEY `role_id` (`role_id`),
   ADD KEY `class_id` (`class_id`);
 
@@ -275,19 +267,19 @@ ALTER TABLE `user_acceptance_request`
 -- AUTO_INCREMENT для таблицы `acceptance`
 --
 ALTER TABLE `acceptance`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `acceptance_class`
 --
 ALTER TABLE `acceptance_class`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `file`
 --
 ALTER TABLE `file`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `role`
@@ -305,13 +297,13 @@ ALTER TABLE `status`
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `user_acceptance_request`
 --
 ALTER TABLE `user_acceptance_request`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -328,7 +320,6 @@ ALTER TABLE `acceptance_class`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`acceptance_id`) REFERENCES `acceptance` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `user_ibfk_3` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
